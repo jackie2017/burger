230576f34305be7e5844639e4ae609f74db26aae
Jacqueline Young <jackiey1966@gmail.com>
2017-04-30 16:33:03 -0400

    pushing to heroku

---
+++ burger_db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burger (burger_name) VALUES ('portibello');
+INSERT INTO burger (burger_name) VALUES ('mushroom');
+INSERT INTO burger (burger_name) VALUES ('bacon');
+INSERT INTO burger (burger_name) VALUES ('lettace-tomatoe');
+INSERT INTO burger (burger_name) VALUES ('onions');
