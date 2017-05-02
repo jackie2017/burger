f78962afe691217a240c3ab4dd63f1b5ec32168a
Jacqueline Young <jackiey1966@gmail.com>
2017-05-02 18:16:24 -0400

    update

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,22 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection ;
+var connection;
 if(process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Begin2017",
+  password: "Begin2017",c
   database: "burger_db"
 });
 };
+connection.connect();
+module.exports = connection;
 
-// Make connection.
+
+/* Make connection.
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -23,4 +26,4 @@ connection.connect(function(err) {
 });
 
 // Export connection for our ORM to use.
-module.exports = connection;
+*/
